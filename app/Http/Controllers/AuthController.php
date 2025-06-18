5b50aa95026d0e5ed4fb308540d082608d545f93
Haidar-Hilmi27 <haidar.hilmi2727@gmail.com>
2025-06-18 08:16:51 +0700

    login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login'); // buat file ini nanti
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('/dashboard'); // ganti sesuai halaman utama
+        }
+
+        return back()->withErrors([
+            'email' => 'Email atau password salah',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        return redirect('/login');
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AuthController;
 
 // Arahkan root "/" ke controller DashboardController@index
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 // Arahkan "/index" juga ke controller yang sama (jika masih ingin mendukung /index)
 Route::get('/index', [DashboardController::class, 'index']);
+
+
+
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+
+
