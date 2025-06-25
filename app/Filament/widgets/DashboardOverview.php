5b969939956cb6374751aca5d808827d8f57a9b6
Haidar-Hilmi27 <haidar.hilmi2727@gmail.com>
2025-06-25 08:22:44 +0700

    dashboard

---
+++ app/Filament/widgets/DashboardOverview.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Card;
+use App\Models\AreaParkir;
+use App\Models\Jenis;
+use App\Models\Kampus;
+use App\Models\Kendaraan;
+use App\Models\Transaksi;
+use Carbon\Carbon;
+
+
+
+
+class DashboardOverview extends BaseWidget
+{
+    protected function getCards(): array
+    {
+        return [
+            Card::make('Total Area Parkir', AreaParkir::count()),
+            Card::make('Total Jenis', Jenis::count()),
+            Card::make('Total Kampus', Kampus::count()),
+            Card::make('Total Kendaraan', Kendaraan::count()),
+            Card::make('Total Transaksi', Transaksi::count()),
+        ];
+    }
+}
